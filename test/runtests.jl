ca976cb7d48b6255fa02d655b969a8548b614f85
Christopher Rackauckas <Me@ChrisRackauckas.com>
2016-06-16 18:58:57 -0700

    Some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,3 +7,10 @@ push!(S,(0.5,0.4,0.3))
 push!(S,(0.5,0.4,0.4))
 reset!(S)
 push!(S,(0.5,0.4,0.3))
+@test S.data[1] == (0.5,0.4,0.3)
+
+S = ResettableStack{}(Float64)
+for i=1:10
+  push!(S,i)
+end
+@test pop!(S) == 10
