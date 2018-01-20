fd28e6a222ce0797c0af8b1c8abeef1ab091cf0f
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-01-19 17:57:32 -0800

    performance

--- src/ResettableStacks.jl
+++ src/ResettableStacks.jl
@@ -7,78 +7,8 @@ module ResettableStacks
   using StaticArrays
 
   import Base: isempty, length, push!, pop!, start, next, done
-  type ResettableStack{T}
-    data::Vector{T}
-    cur::Int
-    numResets::Int
-  end
 
-  ResettableStack{T}(ty::Type{T}) = ResettableStack{T}(Vector{T}(),0,0)
-
-  isempty(S::ResettableStack) = S.cur==0
-  length(S::ResettableStack)  = S.cur
-
-  function push!(S::ResettableStack,x)
-    if S.cur==length(S.data)
-      S.cur+=1
-      push!(S.data,x)
-    else
-      S.cur+=1
-      S.data[S.cur]=x
-    end
-    nothing
-  end
-
-  safecopy(x) = copy(x)
-  safecopy(x::Void) = nothing
-
-  # For DiffEqNoiseProcess S₂ fast updates
-  function copyat_or_push!(S::ResettableStack,x::Tuple{Number,AbstractArray,Union{AbstractArray,Void}})
-    if S.cur==length(S.data)
-      S.cur+=1
-      push!(S.data,safecopy.(x))
-    else
-      S.cur+=1
-      curx = S.data[S.cur]
-      if typeof(curx[2]) <: SArray
-        S.data[S.cur] = (curx[1],x[2],x[3])
-      else
-        curx[2] .= x[2]
-        if x[3] != nothing
-          curx[3] .= x[3]
-        end
-      end
-    end
-    nothing
-  end
-
-  copyat_or_push!(S::ResettableStack,x) = push!(S,x)
-
-  function pop!(S::ResettableStack)
-    if S.cur==length(S.data)
-      S.cur-=1
-      pop!(S.data)
-    else
-      S.cur-=1
-      S.data[S.cur+1]
-    end
-  end
-
-  start(S::ResettableStack) = S.cur
-  function next(S::ResettableStack,s)
-    s -= 1
-    (S.data[s+1],s)
-  end
-  done(S::ResettableStack,s) = s==0
-
-  function reset!(S::ResettableStack,force_reset = false)
-    S.numResets += 1
-    S.cur = 0
-    if S.numResets%FULL_RESET_COUNT==0 || force_reset
-      S.data = Vector{eltype(S.data)}()
-    end
-    nothing
-  end
+  include("core.jl")
 
   export ResettableStack, reset!
 end # module
---
+++ src/core.jl
@@ -0,0 +1,67 @@
+type ResettableStack{T}
+  data::Vector{T}
+  cur::Int
+  numResets::Int
+end
+
+ResettableStack{T}(ty::Type{T}) = ResettableStack{T}(Vector{T}(),0,0)
+
+isempty(S::ResettableStack) = S.cur==0
+length(S::ResettableStack)  = S.cur
+
+function push!(S::ResettableStack,x)
+  if S.cur==length(S.data)
+    S.cur+=1
+    push!(S.data,x)
+  else
+    S.cur+=1
+    S.data[S.cur]=x
+  end
+  nothing
+end
+
+safecopy(x) = copy(x)
+safecopy(x::Union{Number,StaticArray}) = x
+safecopy(x::Void) = nothing
+
+# For DiffEqNoiseProcess S₂ fast updates
+function copyat_or_push!(S::ResettableStack,x)
+  if S.cur==length(S.data)
+    S.cur+=1
+    push!(S.data,safecopy.(x))
+  else
+    S.cur+=1
+    curx = S.data[S.cur]
+    if typeof(curx[2]) <: Union{Number,SArray}
+      S.data[S.cur] = x
+    else
+      curx[2] .= x[2]
+      if x[3] != nothing
+        curx[3] .= x[3]
+      end
+      S.data[S.cur] = (x[1],curx[2],curx[3])
+    end
+  end
+  nothing
+end
+
+function pop!(S::ResettableStack)
+  S.cur-=1
+  S.data[S.cur+1]
+end
+
+start(S::ResettableStack) = S.cur
+function next(S::ResettableStack,s)
+  s -= 1
+  (S.data[s+1],s)
+end
+done(S::ResettableStack,s) = s==0
+
+function reset!(S::ResettableStack,force_reset = false)
+  S.numResets += 1
+  S.cur = 0
+  if length(S.data) > 5 && (S.numResets%FULL_RESET_COUNT==0 || force_reset)
+    resize!(S.data,max(length(S.data)÷2,5))
+  end
+  nothing
+end
